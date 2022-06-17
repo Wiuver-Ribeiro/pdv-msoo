406a2c5a69b563f4d9878e5274024404d8e79761
Wiuver Afonso Ribeiro <52933241+Wiuver-Ribeiro@users.noreply.github.com>
2022-06-17 15:02:23 -0300

    Att em andamento

--- src/Config.php
+++ src/Config.php
@@ -4,11 +4,11 @@ namespace src;
 class Config {
     const BASE_DIR = '/pdv-msoo/public';
 
-    const DB_DRIVER = 'mysql';
-    const DB_HOST = 'localhost';
-    const DB_DATABASE = 'test';
+    const DB_DRIVER = 'mysql'; // Drivers suportados nesta versão [MySQL, PostgreSQL, NoSQL, MongoDB]
+    const DB_HOST = 'localhost'; // Usuário padrão [root]
+    const DB_DATABASE = 'pdv-msoo';
     CONST DB_USER = 'root';
-    const DB_PASS = '';
+    const DB_PASS = ''; // Sem senha [por padrão]
 
     const ERROR_CONTROLLER = 'ErrorController';
     const DEFAULT_ACTION = 'index';
